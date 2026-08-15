26405e36ae5f68f43e4fe2f7130ad9131df82ae2
Andy Fajardo <andy.fajardo@maristascomayagua.edu.hn>
2026-08-15 13:51:24 -0600

    Guardando contenido de Consulta_Platillos2.sql

---
+++ Insert_Tablas.sql/Insert_Platillos.sql
@@ -0,0 +1,11 @@
+INSERT INTO platillos (categoria_id, nombre, descripcion, precio, disponible) VALUES
+(1, 'Nachos Supremos', 'Nachos con queso, guacamole y frijoles', 120.00, true),
+(2, 'Hamburguesa Doble', 'Carne de res, queso cheddar y tocino', 180.00, true),
+(2, 'Tacos al Pastor', 'Orden de 5 tacos con piña y cilantro', 110.00, true),
+(3, 'Pastel de Chocolate', 'Rebanada de pastel húmedo de chocolate', 75.00, true),
+(4, 'Limonada Natural', 'Bebida refrescante de limón recién exprimido', 45.00, true),
+(5, 'Café Americano', 'Café negro recién elaborado', 35.00, true),
+(6, 'Ensalada César', 'Lechuga, pollo a la parrilla, crutones y aderezo', 130.00, true),
+(7, 'Sopa Azteca', 'Sopa de tortilla con aguacate y queso', 90.00, true),
+(8, 'Ceviche de Pescado', 'Pescado curado en limón con pico de gallo', 160.00, true),
+(9, 'Pizza Pepperoni', 'Pizza artesanal de 8 rebanadas', 210.00, true);
