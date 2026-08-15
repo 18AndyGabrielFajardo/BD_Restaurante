7a6d2a2b03ea642560193aad12d40c537a014996
Andy Fajardo <andy.fajardo@maristascomayagua.edu.hn>
2026-08-15 13:44:38 -0600

    Insertar registros en tabla Detalle Pedidos

---
+++ Insert_Tablas.sql/Insert_Detalle_Pedido.sql
@@ -0,0 +1,11 @@
+INSERT INTO detalles_pedido (pedido_id, platillo_id, cantidad, precio_unitario) VALUES
+(1, 2, 1, 180.00), -- Pedido 1: Hamburguesa
+(1, 5, 1, 45.00),  -- Pedido 1: Limonada
+(2, 1, 1, 120.00), -- Pedido 2: Nachos
+(2, 8, 1, 90.00),  -- Pedido 2: Sopa
+(3, 2, 1, 180.00), -- Pedido 3: Hamburguesa
+(4, 10, 2, 210.00),-- Pedido 4: 2 Pizzas Pepperoni
+(5, 7, 1, 130.00), -- Pedido 5: Ensalada César
+(5, 9, 1, 160.00), -- Pedido 5: Ceviche
+(6, 9, 1, 160.00), -- Pedido 6: Ceviche
+(7, 3, 4, 110.00); -- Pedido 7: Tacos al Pastor
